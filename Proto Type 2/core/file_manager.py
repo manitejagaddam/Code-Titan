da77f356ae799e627e64430b843675dd42921b64
Maniteja_Gaddam <happyfamily110705@gmail.com>
2025-10-04 14:34:18 +0530

    filemanager code base is ready

---
+++ Proto Type 2/core/file_manager.py
@@ -0,0 +1,12 @@
+import os
+
+
+class FileManager:
+    
+    def write_file(self, file_name : str, content : str, output_dir = "output") -> None:
+        os.makedirs(output_dir, exist_ok=True)
+        path = os.path.join(output_dir, file_name)
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(content)
+        print(f"💾 Saved: {path}")
+        
